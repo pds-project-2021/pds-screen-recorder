f3545e1a57ca55378282909785d1ea07c30fa3cd
Alessandro <s277945@studenti.polito.it>
2021-09-21 17:58:39 +0200

    preliminary interface layout tests

--- pds-screen-recorder/pds-screen-recorder.cpp
+++ pds-screen-recorder/pds-screen-recorder.cpp
@@ -1,4 +1,5 @@
 #include <gtk/gtk.h>
+#include <iostream>
 
 static void
 print_hello(GtkWidget* widget,
@@ -12,16 +13,26 @@ activate(GtkApplication* app,
     gpointer        user_data)
 {
     GtkWidget* window;
-    GtkWidget* button;
+    GtkWidget* buttonGrid;
+    GtkWidget* recordButton;
+    GtkWidget* pauseButton;
+    GtkWidget* stopButton;
 
     window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "Window");
+    buttonGrid = gtk_grid_new();
+    gtk_window_set_title(GTK_WINDOW(window), "Screen recorder");
     gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
 
-    button = gtk_button_new_with_label("Hello World");
-    g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
-    gtk_window_set_child(GTK_WINDOW(window), button);
-
+    recordButton = gtk_button_new_with_label("Record");
+    pauseButton = gtk_button_new_with_label("Pause");
+    stopButton = gtk_button_new_with_label("Stop");
+    g_signal_connect(recordButton, "clicked", G_CALLBACK(print_hello), NULL);
+    gtk_window_set_child(GTK_WINDOW(window), buttonGrid);
+    gtk_grid_attach(GTK_GRID(buttonGrid), recordButton, 0, 0, 100, 50);
+    //gtk_grid_insert_next_to(GTK_GRID(buttonGrid), recordButton, GTK_POS_RIGHT);
+    gtk_grid_attach_next_to(GTK_GRID(buttonGrid), pauseButton, recordButton, GTK_POS_RIGHT, 100, 50);
+    gtk_grid_attach_next_to(GTK_GRID(buttonGrid), stopButton, pauseButton, GTK_POS_RIGHT, 100, 50);
+    std::cout << gtk_grid_get_baseline_row << "\n";
     gtk_window_present(GTK_WINDOW(window));
 }
 
