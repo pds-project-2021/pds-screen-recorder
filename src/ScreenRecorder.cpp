828aee04c0bfb7e88913185c3efac63f1b5c9749
Alessandro <s277945@studenti.polito.it>
2021-10-26 12:20:59 +0200

    linux input selection wip

--- src/ScreenRecorder.cpp
+++ src/ScreenRecorder.cpp
@@ -77,13 +77,16 @@ int ScreenRecorder::init() {
 //	av_dict_set(&options, "video_size", "1920x1080", 0);
 	av_dict_set(&options, "show_region", "1", 0);
 
-	//inputFormat = av_find_input_format("x11grab");
-	//ret = avformat_open_input(&inputFormatContext, ":0.0+0,0", inputFormat, &options);
-    inputFormat = av_find_input_format("kmsgrab");
-    ret = avformat_open_input(&inputFormatContext, "hwdownload,format=bgr0", inputFormat, &options);
-	if (ret != 0) {
-		throw avException("Couldn't open input stream");
-	}
+#ifdef X11
+	inputFormat = av_find_input_format("x11grab");
+	ret = avformat_open_input(&inputFormatContext, ":0.0+0,0", inputFormat, &options);
+#else
+    inputFormat = av_find_input_format("video4linux2");
+    ret = avformat_open_input(&inputFormatContext, "/dev/video0", inputFormat, &options);
+#endif
+    if (ret != 0) {
+        throw avException("Couldn't open input stream");
+    }
 #else
 	show_avfoundation_device();
 	inputFormat = av_find_input_format("avfoundation");
